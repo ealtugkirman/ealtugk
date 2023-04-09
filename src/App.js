cffdad5550eb5ddd1b89b4b288532b0191fac591
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2023-04-09 03:16:48 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ import About from './components/About';
 import Services from './components/Services';
 import Work from './components/Work';
 import { ContactUs } from './components/ContactUs';
+import Work2 from './components/Work2';
 
 const App = () => {
   return (
-    <div className='bg-site bg-no-repeat lg:pr-20 lg:pl-20 bg-cover overflow-hidden'>
+    <div className='bg-site bg-no-repeat lg:pr-20 py-40 lg:pl-20 bg-cover overflow-hidden'>
       <Header />
       <Banner />
       <Nav />
       <Work />
+      <Work2 />
       <About />
       <Services />
       <ContactUs />
